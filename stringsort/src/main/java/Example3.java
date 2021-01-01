6acf9321b0f01102170a57f2f34f97cec914a088
Carlos Munoz <carlos.munoz@cornershop.mx>
2020-12-31 18:04:10 -0600

    Java example 3

---
+++ stringsort/src/main/java/Example3.java
@@ -0,0 +1,20 @@
+class Example3 {
+    public static void main (String[] args) {
+        int v;
+        double x;
+
+        v = 10;
+        x = 10;
+
+        System.out.println("Original value of v: " + v);
+        System.out.println("Original value of x: " + x);
+
+        // Divide values by four
+        v = v / 4;
+        x = x / 4;
+
+        System.out.println();
+        System.out.println("Value after division: " + v);
+        System.out.println("Value after division: " + x);
+    }
+}
