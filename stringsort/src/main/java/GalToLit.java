d19b6986a549a3c1a6d14c58850197e4c57bd096
Carlos Munoz <carlos.munoz@cornershop.mx>
2020-12-31 18:08:08 -0600

    Gallons to liters

---
+++ stringsort/src/main/java/GalToLit.java
@@ -0,0 +1,11 @@
+class GalToLit {
+    public static void main (String[] args) {
+        double gallons;
+        double liters;
+
+        gallons = 10;
+        liters = gallons * 3.7854;
+
+        System.out.println("There are " + liters + " in " + gallons);
+    }
+}
